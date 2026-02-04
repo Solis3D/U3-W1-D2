605d4835686873c949cf22b2435bdfadb42384ff
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-02-04 16:26:43 +0100

    U3-W1-D3

--- src/components/MyLibrary.jsx
+++ src/components/MyLibrary.jsx
@@ -1,22 +1,11 @@
-import { Row, Col } from "react-bootstrap";
-import libreria from "../data/horror.json";
-import Card from "react-bootstrap/Card";
+import { Row } from "react-bootstrap";
+import SingleBook from "./SingleBook";
 
-const MyLibrary = function () {
+const MyLibrary = function (props) {
   return (
     <Row className=" justify-content-center g-2 gap-3 my-5">
-      {libreria.map((libro) => {
-        return (
-          <Col key={libro.asin} xs="auto">
-            <Card className=" h-100" style={{ width: "14rem" }}>
-              <Card.Img variant="top" src={libro.img} style={{ height: "300px" }} />
-              <Card.Body className=" d-flex flex-column justify-content-between">
-                <Card.Title>{libro.title}]</Card.Title>
-                <Card.Text className=" align-self-end">{libro.price}€</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        );
+      {props.books.map((libro) => {
+        return <SingleBook key={libro.asin} book={libro} />;
       })}
     </Row>
   );
---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,45 @@
+import { Col } from "react-bootstrap";
+import { Component } from "react";
+import Card from "react-bootstrap/Card";
+
+// const SingleBook = function (props) {
+//   return (
+//     <Col key={props.book.asin} xs="auto">
+//       <Card className=" h-100" style={{ width: "14rem" }}>
+//         <Card.Img variant="top" src={props.book.img} style={{ height: "300px" }} />
+//         <Card.Body className=" d-flex flex-column justify-content-between">
+//           <Card.Title>{props.book.title}]</Card.Title>
+//           <Card.Text className=" align-self-end">{props.book.price}€</Card.Text>
+//         </Card.Body>
+//       </Card>
+//     </Col>
+//   );
+// };
+
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  toggleSelected = () => {
+    this.setState((prevState) => ({
+      selected: !prevState.selected,
+    }));
+  };
+
+  render() {
+    return (
+      <Col xs="auto">
+        <Card className={`h-100 single-book ${this.state.selected ? "selected-book" : ""}`} style={{ width: "14rem" }} onClick={this.toggleSelected}>
+          <Card.Img variant="top" src={this.props.book.img} style={{ height: "300px" }} />
+          <Card.Body className=" d-flex flex-column justify-content-between">
+            <Card.Title>{this.props.book.title}</Card.Title>
+            <Card.Text className=" align-self-end">{this.props.book.price}€</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+}
+
+export default SingleBook;
